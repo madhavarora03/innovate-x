57877557f06dbeef529697a0b90f85c1ed596a33
madhavarora03 <eragon.cube@outlook.com>
2024-07-20 06:17:59 +0530

    Initial frontend setup with react + tailwind + daisy-ui

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <h1 className="text-slate-700" >
+      <div>Hello!</div>
+      <button className="btn btn-primary">Button</button>
+    </h1>
+  );
+}
+
+export default App;
